f7800cce5bce9650427653398c342de987f9613d
Romain Lanz <romain.lanz@slynova.ch>
2018-09-17 19:08:43 +0200

    feat(auth): reset password

--- app/Controllers/Http/Admin/PasswordRequestController.js
+++ app/Controllers/Http/Admin/PasswordRequestController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Persona = use('Persona')
+const Encryption = use('Encryption')
 
 class PasswordRequestController {
   async store ({ request, response }) {
@@ -10,7 +11,7 @@ class PasswordRequestController {
   }
 
   async update ({ params, request, response }) {
-    const token = params.token
+    const token = Encryption.base64Decode(params.token)
     const payload = request.only(['password', 'password_confirmation'])
 
     await Persona.updatePasswordByToken(token, payload)
--- app/Models/User.js
+++ app/Models/User.js
@@ -1,7 +1,7 @@
 'use strict'
 
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
+const Hash = use('Hash')
 
 class User extends Model {
   static boot () {
@@ -10,11 +10,12 @@ class User extends Model {
     /**
      * A hook to hash the user password before saving
      * it to the database.
-     *
-     * Look at `app/Models/Hooks/User.js` file to
-     * check the hashPassword method
      */
-    this.addHook('beforeCreate', 'User.hashPassword')
+    this.addHook('beforeSave', async (userInstance) => {
+      if (userInstance.dirty.password) {
+        userInstance.password = await Hash.make(userInstance.password)
+      }
+    })
   }
 
   static get hidden () {
