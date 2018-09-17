fb8f5f1bcab26fbebe252ceadc492f8f33a895af
Romain Lanz <romain.lanz@slynova.ch>
2018-09-17 13:35:20 +0200

    fix(route): fix route order

--- start/routes.js
+++ start/routes.js
@@ -14,8 +14,8 @@ Route.get('articles/:id', 'ArticleController.show')
 Route.get('articles', 'ArticleController.index')
 
 Route.get('teams', 'TeamController.index')
-Route.get('teams/:id', 'TeamController.show')
 Route.get('teams/categories', 'TeamCategoryController.index')
+Route.get('teams/:id', 'TeamController.show')
 
 Route.group(() => {
   Route.get('me', 'UserController.current')
