8ee127e6123ad49b096ff10fd0b70ce4438206ee
Romain Lanz <romain.lanz@slynova.ch>
2018-09-17 14:37:47 +0200

    fix(players): send social only when available

--- app/Controllers/Http/TeamController.js
+++ app/Controllers/Http/TeamController.js
@@ -19,17 +19,36 @@ class TeamController {
     const team = (await Team.query().where('id', params.id).where('activated', true).with('category').with('members').first()).toJSON()
 
     team.players = team.members.map((member) => {
+      const socials = []
+
+      if (member.twitter) {
+        socials.push({ type: 'twitter', url: `https://twitter.com/${member.twitter}` })
+      }
+
+      if (member.facebook) {
+        socials.push({ type: 'facebook', url: `https://www.facebook.com/${member.facebook}` })
+      }
+
+      if (member.twitch) {
+        socials.push({ type: 'twitch', url: `https://www.twitch.tv/${member.twitch}` })
+      }
+
+      if (member.youtube) {
+        socials.push({ type: 'youtube', url: `https://www.youtube.com/channel/${member.youtube}` })
+      }
+
+      if (member.battletag) {
+        socials.push({ type: 'bnet', url: member.battletag })
+      }
+
+      if (member.steam) {
+        socials.push({ type: 'steam', url: `https://steamcommunity.com/id/${member.steam}` })
+      }
+
       return {
         ...pick(member, ['nickname', 'lastname', 'firstname']),
         role: member.pivot.role,
-        socials: [
-          { type: 'twitter', url: `https://twitter.com/${member.twitter}` },
-          { type: 'facebook', url: `https://www.facebook.com/${member.facebook}` },
-          { type: 'twitch', url: `https://www.twitch.tv/${member.twitch}` },
-          { type: 'youtube', url: `https://www.youtube.com/channel/${member.youtube}` },
-          { type: 'bnet', url: member.battletag },
-          { type: 'steam', url: `https://steamcommunity.com/id/${member.steam}` },
-        ]
+        socials
       }
     })
 
