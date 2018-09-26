e6621d09097beea6c64b94926012a16ec3a9b4c0
Romain Lanz <romain.lanz@slynova.ch>
2018-09-26 09:04:16 +0200

    chore(config): trust proxy

--- config/app.js
+++ config/app.js
@@ -40,7 +40,7 @@ module.exports = {
     | values. Read documentation for that.
     |
     */
-    trustProxy: false,
+    trustProxy: true,
 
     /*
     |--------------------------------------------------------------------------
