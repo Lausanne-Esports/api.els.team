c6355c56261e7c97302d38e96d0913c27212533a
Romain Lanz <romain.lanz@slynova.ch>
2019-01-07 08:02:47 +0100

    feat(teams): use transformers

--- app/Controllers/Http/TeamController.js
+++ app/Controllers/Http/TeamController.js
@@ -7,61 +7,20 @@
  * @copyright Lausanne-Sport eSports - Romain Lanz
  */
 
-const { pick } = require('lodash')
 const Team = use('App/Models/Team')
+const TeamTransformer = use('App/Transformers/TeamTransformer')
 
 class TeamController {
-  async index () {
-    const teams = (
-      await Team.query().with('category').with('members').where('activated', true).fetch()
-    ).toJSON()
+  async index ({ transform }) {
+    const teams = await Team.query().with('category').with('members').where('activated', true).fetch()
 
-    return teams.map(team => ({
-      ...pick(team, ['id', 'name', 'order', 'category', 'academy']),
-      numberOfPlayers: team.members.length
-    }))
+    return transform.collection(teams, TeamTransformer)
   }
 
-  async show ({ params }) {
-    const team = (await Team.query().where('id', params.id).where('activated', true).with('category').with('members').first()).toJSON()
+  async show ({ params, transform }) {
+    const team = await Team.query().where('id', params.id).where('activated', true).with('category').with('members').first()
 
-    team.players = team.members.map((member) => {
-      const socials = []
-
-      if (member.twitter) {
-        socials.push({ type: 'twitter', url: `https://twitter.com/${member.twitter}` })
-      }
-
-      if (member.facebook) {
-        socials.push({ type: 'facebook', url: `https://www.facebook.com/${member.facebook}` })
-      }
-
-      if (member.twitch) {
-        socials.push({ type: 'twitch', url: `https://www.twitch.tv/${member.twitch}` })
-      }
-
-      if (member.youtube) {
-        socials.push({ type: 'youtube', url: `https://www.youtube.com/channel/${member.youtube}` })
-      }
-
-      if (member.battletag) {
-        socials.push({ type: 'bnet', url: member.battletag })
-      }
-
-      if (member.steam) {
-        socials.push({ type: 'steam', url: `https://steamcommunity.com/id/${member.steam}` })
-      }
-
-      return {
-        ...pick(member, ['nickname', 'lastname', 'firstname', 'picture']),
-        role: member.pivot.role,
-        socials
-      }
-    })
-
-    return {
-      ...pick(team, ['id', 'name', 'order', 'academy', 'players', 'category'])
-    }
+    return transform.include('players').item(team, TeamTransformer)
   }
 }
 
---
+++ app/Transformers/MemberTransformer.js
@@ -0,0 +1,51 @@
+'use strict'
+
+/**
+ * Lausanne-Sport eSports API Source Code.
+ *
+ * @license GPLv3
+ * @copyright Lausanne-Sport eSports - Romain Lanz
+ */
+
+const TransformerAbstract = use('TransformerAbstract')
+
+class MemberTransformer extends TransformerAbstract {
+  transform (member) {
+    const socials = []
+
+    if (member.twitter) {
+      socials.push({ type: 'twitter', url: `https://twitter.com/${member.twitter}` })
+    }
+
+    if (member.facebook) {
+      socials.push({ type: 'facebook', url: `https://www.facebook.com/${member.facebook}` })
+    }
+
+    if (member.twitch) {
+      socials.push({ type: 'twitch', url: `https://www.twitch.tv/${member.twitch}` })
+    }
+
+    if (member.youtube) {
+      socials.push({ type: 'youtube', url: `https://www.youtube.com/channel/${member.youtube}` })
+    }
+
+    if (member.battletag) {
+      socials.push({ type: 'bnet', url: member.battletag })
+    }
+
+    if (member.steam) {
+      socials.push({ type: 'steam', url: `https://steamcommunity.com/id/${member.steam}` })
+    }
+
+    return {
+      nickname: member.nickname,
+      lastname: member.lastname,
+      firstname: member.firstname,
+      picture: member.picture,
+      role: member.getRelated('pivot').role,
+      socials
+    }
+  }
+}
+
+module.exports = MemberTransformer
