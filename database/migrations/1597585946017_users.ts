560cded754c040a32991e272e052541114827866
Valentin Kaelin <kalanehd@gmail.com>
2020-08-17 15:12:00 +0200

    chore: add adonis auth and session packages

--- database/migrations/1597585946017_users.ts
+++ database/migrations/1597585946017_users.ts
@@ -14,9 +14,10 @@ export default class Users extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.string('username', 80).notNullable().unique()
-      table.string('email', 254).notNullable().unique()
-      table.string('password', 255).notNullable()
+      table.string('email', 255).notNullable().unique()
+      table.string('password', 180).notNullable()
       table.string('account_status')
+      table.string('remember_me_token').nullable()
       table.timestamps(true)
     })
   }
