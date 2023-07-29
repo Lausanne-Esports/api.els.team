6161651943ec2f591e5d905d936d63c06ec031f0
Kalane <vkaelin@hey.com>
2023-07-29 17:25:47 +0200

    feat: update streams admin dashboard to helix version

--- app/Transformers/StreamTransformer.ts
+++ app/Transformers/StreamTransformer.ts
@@ -15,8 +15,6 @@ export interface SerializedStream {
   username: string,
   display_name: string,
   is_live?: boolean,
-  is_partner?: boolean,
-  logo?: string,
   viewers?: number,
   status?: string,
   game?: string,
@@ -26,20 +24,14 @@ export interface SerializedStream {
 class StreamTransformer {
   public transform (stream: SerializedStream, twitchStreams: TwitchStream[]) {
     if (twitchStreams && twitchStreams[stream.username] !== undefined) {
-      const twitchStream = twitchStreams[stream.username]
+      const twitchStream: TwitchStream = twitchStreams[stream.username]
 
       stream.is_live = true
-      stream.is_partner = twitchStream.channel.partner
-      stream.logo = twitchStream.channel.logo
-      stream.viewers = twitchStream.viewers
-      stream.status = twitchStream.channel.status
-      stream.game = twitchStream.game
-      stream.preview = twitchStream.preview.large
+      stream.viewers = twitchStream.viewer_count
+      stream.game = twitchStream.game_name
+      stream.preview = twitchStream.thumbnail_url.replace('{width}', '320').replace('{height}', '180')
     } else {
       stream.is_live = false
-      stream.is_partner = false
-      stream.logo = ''
-      stream.status = ''
       stream.viewers = 0
       stream.game = ''
       stream.preview = ''
--- env.ts
+++ env.ts
@@ -39,4 +39,5 @@ export default Env.rules({
   SMTP_PASSWORD: Env.schema.string(),
 
   TWITCH_API_KEY: Env.schema.string(),
+  TWITCH_API_SECRET: Env.schema.string(),
 })
